9ec0aa59832e5b3d6ba5da88f376845e9883e8a0
MariliaNeves <marilianeves68@gmail.com>
2018-11-09 09:41:41 -0300

    Concatenação de arquivos .js com Gulp

--- projeto/gulpfile.js
+++ projeto/gulpfile.js
@@ -1,7 +1,8 @@
 // gulpfile.js
 var gulp = require('gulp')
   ,imagemin = require('gulp-imagemin'),
-  clean = require('gulp-clean');
+  clean = require('gulp-clean'),
+  concat = require('gulp-concat');
 
 gulp.task('copy', ['clean'], function(){
 
@@ -21,4 +22,13 @@ gulp.task('build-img',['copy'], function() {
   gulp.src('src/img/**/*')
     .pipe(imagemin()) //minifica imagens para economizar banda do usuario
     .pipe(gulp.dest('src/img'));
+});
+
+/*junta todos os arquivos .js e concatena salvando na pasta dist. 
+Um unico arquivo para ser carregado no navegador do usuario o que melhora o desempenho*/
+gulp.task('build-js', function() {
+
+    gulp.src('dist/js/**/*.js')
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('dist/js'));
 });
